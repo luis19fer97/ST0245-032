17123de2cae4618641005638565c57fdba0ea4c4
luis19fer97 <50598879+luis19fer97@users.noreply.github.com>
2020-02-05 23:35:12 -0500

    Punto 2

--- talleres/taller03/Taller_3.py
+++ talleres/taller03/Taller_3.py
@@ -5,3 +5,17 @@ def hanoi(n, a = "Tower 1", b = "Tower 2", c = "Tower 3"):
         hanoi(n - 1, a, c , b )
         print ("From " + a + " to " + c)
         hanoi(n - 1, b, a, c )
+        
+def permutacion(perm, s):
+    
+       if len(s) == len(perm): 
+           yield s # Se usa yield para que luego de la primera permutación no se salga de la función
+       for i in perm:
+           if i in s: 
+               continue
+           s=s+i
+           for x in permutacion(perm, s): 
+               yield x
+           s=s[:-1]
+               
+print(list(permutacion("abc",'')))
