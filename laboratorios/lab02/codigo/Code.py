735e4259e5fd9e61644749f82b3eb32e2b8fac40
luis19fer97 <50598879+luis19fer97@users.noreply.github.com>
2020-03-01 22:42:45 -0500

    Update Code.py
    Códigos con ciclo para almacenar tiempos

--- laboratorios/lab02/codigo/Code.py
+++ laboratorios/lab02/codigo/Code.py
@@ -1,22 +1,36 @@
+import numpy as np
+import sys
+import time
+
+#Inicializar variables
+sys.setrecursionlimit(1000000)
+start_time = time.time()
+out = np.empty([20, 1])
+a = 0
+
 # Tomado de https://www.geeksforgeeks.org/python-program-for-insertion-sort/ 
-def insertionSort(arr): 
-    # Traverse through 1 to len(arr) 
-    for i in range(1, len(arr)): 
+def insertionSort(x): 
+    #start_time = time.time() #Iniciar tiempo de ejecución
+    
+    # Traverse through 1 to len(x) 
+    for i in range(1, len(x)): 
   
-        key = arr[i] 
+        key = x[i] 
   
-        # Move elements of arr[0..i-1], that are 
+        # Move elements of x[0..i-1], that are 
         # greater than key, to one position ahead 
         # of their current position 
         j = i-1
-        while j >=0 and key < arr[j] : 
-                arr[j+1] = arr[j] 
+        while j >=0 and key < x[j] : 
+                x[j+1] = x[j] 
                 j -= 1
-        arr[j+1] = key 
+        x[j+1] = key 
+    #return ((time.time() - start_time)*1000) #Retornar tiempo de ejecución en milisegundos
 
-#%%
 # Tomado de https://www.geeksforgeeks.org/merge-sort/
 def mergeSort(x): 
+    #start_time = time.time() #Iniciar tiempo de ejecución
+
     if len(x) >1: 
         mid = len(x)//2 #Finding the mid of the xay 
         L = x[:mid] # Dividing the xay elements  
@@ -47,7 +61,18 @@ def mergeSort(x):
             x[k] = R[j] 
             j+=1
             k+=1
-    return x
+            
+    return (x)
+    #return ((time.time() - start_time)*1000) #Retornar tiempo de ejecución en milisegundos
 
+"""
+#Ciclo para almacenar los tiempos de ejecución de los algoritmos en un arreglo
+for i in range (500, 10500, 500):
+    array = np.random.randint(1,i,i)
+    #out[a,0] =  mergeSort(array)
+    out[a,0] =  insertionSort(array)
+    a = a +1
+print (out)
+"""
 
   
